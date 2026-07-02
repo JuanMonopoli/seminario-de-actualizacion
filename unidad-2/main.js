24d4efbd542ca0f253d13c12091e7ecada5bd9a1
Juan Monopoli <juan.monopoli1@gmail.com>
2026-07-02 19:12:54 -0300

    Delete unidad-2 directory

--- unidad-2/main.js
+++
@@ -1,9 +0,0 @@
-function main() {
-    const vista = document.querySelector('x-calculadora');
-    const modelo = new CalculatorModel();
-    const controlador = new CalculatorController(vista, modelo);
-
-    controlador.enable();
-}
-
-window.onload = main;
